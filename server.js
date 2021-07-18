71c4bcb05ef6f14ff13d68c4d883e4931b59e97a
Ian <onyangoian71@gmail.com>
2021-07-18 13:16:16 +0300

    router set-up

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const indexRoute = require('./routes/index')
+
+app.use('/', indexRoute)
+
+const port =process.env.PORT || 3000;
+app.listen(port, ()=> console.log(`app running on ${port}`))
